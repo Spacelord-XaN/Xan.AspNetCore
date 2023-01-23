10d49415a6d390ca1ed93b5f79114f516defd08b
Alexander Kastler <github@xaka.eu>
2023-01-23 21:24:28 +0100

    Add SelectModel

---
+++ src/Xan.AspNetCore/Models/SelectModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Xan.AspNetCore.Models;
+
+#nullable disable warnings
+public sealed class SelectModel<T>
+{
+    public SelectModel()
+    { }
+
+    public SelectModel(T value)
+    {
+        Value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public bool IsSelected { get; set; }
+
+    public T Value { get; set; }
+}
+
+public static class SelectModelExtensions
+{
+    public static IEnumerable<T> SelectedModels<T>(this IEnumerable<SelectModel<T>> selectModels)
+    {
+        ArgumentNullException.ThrowIfNull(selectModels);
+
+        return selectModels.Where(sm => sm.IsSelected)
+            .Select(sm => sm.Value);
+    }
+
+    public static IEnumerable<SelectModel<T>> ToSelectedModels<T>(this IEnumerable<T> items)
+    {
+        ArgumentNullException.ThrowIfNull(items);
+
+        return items.Select(item => new SelectModel<T>(item));
+    }
+}
