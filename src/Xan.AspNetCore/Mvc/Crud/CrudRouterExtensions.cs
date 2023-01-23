380fa88e6406617d50e65d5b308a9a93202ff072
Alexander Kastler <github@xaka.eu>
2023-01-23 21:26:04 +0100

    Add extension method for ICrudRouter: ToDeleteOrToggleState

---
+++ src/Xan.AspNetCore/Mvc/Crud/CrudRouterExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Xan.AspNetCore.Models;
+using Xan.AspNetCore.Mvc.Abstractions;
+
+namespace Xan.AspNetCore.Mvc.Crud;
+
+public static class CrudRouterExtensions
+{
+    public static string ToDeleteOrToggleState<TEntity>(this ICrudRouter router, CrudItemModel<TEntity> crudItem)
+        where TEntity : ICrudEntity
+    {
+        ArgumentNullException.ThrowIfNull(router);
+        ArgumentNullException.ThrowIfNull(crudItem);
+
+        if (crudItem.CanDelete)
+        {
+            return router.ToDelete(crudItem.Entity.Id);
+        }
+
+        return crudItem.Entity.State switch
+        {
+            ObjectState.Disabled => router.ToEnable(crudItem.Entity.Id),
+            ObjectState.Enabled => router.ToDisable(crudItem.Entity.Id),
+            _ => throw new UnreachableException(),
+        };
+    }
+}
