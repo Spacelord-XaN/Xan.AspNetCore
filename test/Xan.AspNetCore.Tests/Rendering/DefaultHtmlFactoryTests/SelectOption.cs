afe84881b6206d862c218a9b182d2e4285882cf3
Alexander Kastler <github@xaka.eu>
2023-12-08 08:44:11 +0100

    Add tests for DefaultHtmlFactory SelectOption

---
+++ test/Xan.AspNetCore.Tests/Rendering/DefaultHtmlFactoryTests/SelectOption.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Xan.AspNetCore.Rendering;
+
+namespace Xan.AspNetCore.Tests.Rendering.DefaultHtmlFactoryTests;
+
+public class SelectOption
+{
+    [Fact]
+    public void ValueAndSelected_ReturnsHtml()
+    {
+        //  Arrange
+        DefaultHtmlFactory sut = new(Mockups.StringLocalizer());
+
+        //  Act
+        TagBuilder option = sut.SelectOption("ThisIsTheText", "ThisIsTheValue", true);
+
+        //  Assert
+        option.Should().Html("""<option selected="" value="ThisIsTheValue">ThisIsTheText</option>""");
+    }
+
+    [Fact]
+    public void ValueAndNotSelected_ReturnsHtml()
+    {
+        //  Arrange
+        DefaultHtmlFactory sut = new(Mockups.StringLocalizer());
+
+        //  Act
+        TagBuilder option = sut.SelectOption("ThisIsTheText", "ThisIsTheValue", false);
+
+        //  Assert
+        option.Should().Html("""<option value="ThisIsTheValue">ThisIsTheText</option>""");
+    }
+
+    [Fact]
+    public void NullValueAndNotSelected_ReturnsHtml()
+    {
+        //  Arrange
+        DefaultHtmlFactory sut = new(Mockups.StringLocalizer());
+
+        //  Act
+        TagBuilder option = sut.SelectOption("ThisIsTheText", null, false);
+
+        //  Assert
+        option.Should().Html("""<option value="null">ThisIsTheText</option>""");
+    }
+
+    [Fact]
+    public void NullValueAndSelected_ReturnsHtml()
+    {
+        //  Arrange
+        DefaultHtmlFactory sut = new(Mockups.StringLocalizer());
+
+        //  Act
+        TagBuilder option = sut.SelectOption("ThisIsTheText", null, true);
+
+        //  Assert
+        option.Should().Html("""<option selected="" value="null">ThisIsTheText</option>""");
+    }
+}
